20ebe9b6350e879a37f5f62cc6d341905ce7491b
ElektroDed <vasyakushnarenko@gmail.com>
2024-10-15 18:53:33 +0300

    Create OtvetNaPrakticheskoeZadanie_Peremennie.py

---
+++ OtvetNaPrakticheskoeZadanie_Peremennie.py
@@ -0,0 +1,8 @@
+Kolichestvo_vipolnennih_DZ = 12
+Kolichestvo_potrachennih_chasov = 1.5
+Nazvanie_kursa = 'Python'
+Vremya_na_zadanie = Kolichestvo_potrachennih_chasov * 60 / Kolichestvo_vipolnennih_DZ
+print('Количество выполненных ДЗ:', Kolichestvo_vipolnennih_DZ,
+      'Количество затраченных часов:', Kolichestvo_potrachennih_chasov,
+      'Название курса:', Nazvanie_kursa,
+      'Время на одно задание:',Vremya_na_zadanie, 'минут')
