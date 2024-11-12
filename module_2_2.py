c3395b228519656de40d2746fe641d08a412b4d2
ElektroDed <vasyakushnarenko@gmail.com>
2024-11-12 20:57:48 +0300

    Create module_2_2.py

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = input('Введите first:')
+second = input('Введите second:')
+third= input('Введите third:')
+if first == second == third:
+    print (3, ' Совпадения')
+elif first == second or second == third:
+    print(2, ' Совпадения')
+else:
+    print(0, ' Совпадений')
