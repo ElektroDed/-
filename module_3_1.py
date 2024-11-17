4dfbf97563f96178a8752134b964eedbe85f4467
ElektroDed <vasyakushnarenko@gmail.com>
2024-11-17 19:21:53 +0300

    Create module_3_1.py

---
+++ module_3_1.py
@@ -0,0 +1,22 @@
+calls = 0
+def count_calls():
+    global calls
+    calls = calls + 1
+def string_info(string):
+    string_info = [len(string), string.upper(), string.lower()]
+    count_calls()
+    return string_info
+def is_contains(string, list_to_search):
+    if string.lower() in (item.lower() for item in list_to_search):
+        t = True
+        count_calls()
+        return t
+    else:
+        t = False
+        count_calls()
+        return t
+print(string_info('Василий'))
+print(string_info('Александр'))
+print(is_contains('Дом', ['доМ', 'Квартира', 'дАчА']))
+print(is_contains('колесо', ['Дверь', 'МотОР']))
+print(calls)
