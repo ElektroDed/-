dcc33e73617d3c1293d122f700ad104938d8e949
ElektroDed <vasyakushnarenko@gmail.com>
2024-11-25 19:51:39 +0300

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,7 @@ second = input('Введите second:')
 third= input('Введите third:')
 if first == second == third:
     print (3, ' Совпадения')
-elif first == second or second == third:
+elif first == second or second == third or first == third:
     print(2, ' Совпадения')
 else:
     print(0, ' Совпадений')
